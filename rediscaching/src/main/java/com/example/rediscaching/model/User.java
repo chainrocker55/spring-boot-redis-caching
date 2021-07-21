14b9ab0c857876c5d5f1d4212befa7d7840ce6ed
kachain jantalat <kachain@scb10x.com>
2021-07-21 15:59:38 +0700

    Fixed bugs

--- rediscaching/src/main/java/com/example/rediscaching/model/User.java
+++ rediscaching/src/main/java/com/example/rediscaching/model/User.java
@@ -3,13 +3,15 @@ package com.example.rediscaching.model;
 import lombok.Data;
 import lombok.ToString;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.io.Serializable;
+
 @Data
-@Entity
 @ToString
+@Entity
 public class User implements Serializable{
         @Id
         @GeneratedValue
