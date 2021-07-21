f8d492313a954dfc188631777a119bc9e8a1fd06
kachain jantalat <kachain@scb10x.com>
2021-07-21 14:28:10 +0700

    initial

---
+++ rediscaching/src/main/java/com/example/rediscaching/RediscachingApplication.java
@@ -0,0 +1,13 @@
+package com.example.rediscaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RediscachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RediscachingApplication.class, args);
+	}
+
+}
